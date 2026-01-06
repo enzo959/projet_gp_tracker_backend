6d8d0c186a54048566fa830c205095ce1720252e
Enzo Courvalet <courvaletenzo@gmail.com>
2026-01-06 18:15:54 +0100

    add router http chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	//chi
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,9 +18,14 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", healthHandler)
+	r := chi.NewRouter()
+
+	// middlewares de base
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", healthHandler)
 
 	log.Println("Le serveur se lance sur :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
